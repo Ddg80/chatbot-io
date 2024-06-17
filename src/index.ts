91a70de0f2b6f582d5f3ee89d47d5eead16cb351
Ddg80 <dguillin80@gmail.com>
2024-06-17 11:36:05 +0200

    (feat): Add navBar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
 import './index.scss';
 
-const hello = 'hello';
+const Template = class {
+  el: HTMLBodyElement | null;
 
-console.log(hello);
+  messageAlert:string;
+
+  constructor() {
+    this.el = document.querySelector('body');
+    this.messageAlert = 'sdfsdf s dfs dfs d';
+  }
+
+  render() {
+    return `
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Contacts</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    `;
+  }
+
+  run() {
+    this.el!.innerHTML = this.render();
+  }
+};
+
+const template = new Template();
+template.run();
